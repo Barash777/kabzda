fc7abce68b0940b73c473e9433b27cb180e9894d
Barash777 <barash.vertihvost@gmail.com>
2022-05-21 15:43:24 +0300

    lesson 3 with homework

--- src/App.tsx
+++ src/App.tsx
@@ -1,72 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Accordion from "./components/Accordion/Accordion";
+import {Rating} from "./components/Rating/Rating";
 
 function App() {
 
     return (
         <div>
-            <AppTitle/>
-            <Rating/>
-            <Accordion/>
+            <PageTitle title={"This is page title 1"}/>
+            <PageTitle title={"Second title"}/>
+            Article 1
+            <Rating value={2}/>
+            <Accordion title={"first ACCORDION title"}/>
+            <Accordion title={"second ACCORDION title"}/>
+            Article 2
+            <Rating value={4}/>
         </div>
     );
 }
 
-function hey() {
-    return <>asd</>
-}
-
-function AppTitle() {
+function PageTitle(props: any) {
     return (
-        <>This is APP component</>
+        <div>{ props.title }</div>
     );
 }
 
-function Rating() {
-    return (
-        <div>
-            {/*<h4>Star</h4>
-            <h4>Star</h4>
-            <h4>Star</h4>
-            <h4>Star</h4>
-            <h4>Star</h4>*/}
-            <Star/>
-            <Star/>
-            <Star/>
-            <Star/>
-            <Star/>
-        </div>
-    );
-}
-
-function Accordion() {
-    return (
-        <div>
-            <AccordionTitle/>
-            <AccordionBody/>
-        </div>
-    );
-}
-
-function AccordionTitle() {
-    return <h3>MENU</h3>
-}
-
-function AccordionBody() {
-    return (
-        <ul>
-            <li>1</li>
-            <li>2</li>
-            <li>3</li>
-        </ul>
-    )
-}
-
-function Star() {
-    return (
-        <h4>Star</h4>
-    );
-}
 
 export default App;
